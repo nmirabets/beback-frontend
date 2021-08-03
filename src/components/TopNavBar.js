8b8ca3f915214f247accf0e46ff7ee8cf8004791
Nicolás <nmirabets@me.com>
2021-08-03 14:28:47 +0200

    fixed TopNavBar bug

--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -13,10 +13,12 @@ function TopNavBar(props) {
   }
   // fixed top-0 inset-x-0  
   return (
-    <div className= "fixed inset-x-0 flex h-15 py-2 items-center justify-center border-gray-600 border border-b-1 bg-primary bg-opacity-20">
-      <LeftComponent title={props.leftTitle} className="flex w-1/4 m-4" onClick={onClickLeft} />
-      <h1 className="flex justify-center flex-grow w-1/2 text-2xl font-extralight" >{props.mainTitle}</h1>
-      <RightComponent title={props.rightTitle} className="flex w-1/4 justify-end m-4 " onClick={onClickRight} />
+    <div className="bg-white">
+      <div className= "fixed inset-x-0 flex h-15 py-2 items-center justify-center border-gray-600 border border-b-1 bg-primary bg-opacity-20">
+        <LeftComponent title={props.leftTitle} className="flex w-1/4 m-4" onClick={onClickLeft} />
+        <h1 className="flex justify-center flex-grow w-1/2 text-2xl font-extralight" >{props.mainTitle}</h1>
+        <RightComponent title={props.rightTitle} className="flex w-1/4 justify-end m-4 " onClick={onClickRight} />
+      </div>
     </div>
   );
 }
