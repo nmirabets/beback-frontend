54dd1e24f2a8cb9f81f798910fb8ff998a0d8c2d
Nicolás <nmirabets@me.com>
2021-08-05 17:06:06 +0200

    fixed TopNavBar transparency bug

--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -13,13 +13,15 @@ function TopNavBar(props) {
   }
   // fixed top-0 inset-x-0  
   return (
-    <div className= "fixed inset-x-0 flex h-14 items-center justify-center border border-b-1 border-primary bg-primary bg-opacity-20 pt-2">
-      <div className="w-1/3 flex justify-start" >
-        <LeftComponent title={props.leftTitle} onClick={onClickLeft} />
-      </div>
-      <h1 className="flex justify-center w-1/3 text-sm font-normal" >{props.mainTitle}</h1>
-      <div className="w-1/3 flex justify-end" >
-        <RightComponent title={props.rightTitle} onClick={onClickRight} />
+    <div className="bg-white">
+      <div className= "fixed inset-x-0 flex h-14 items-center justify-center border border-b-1 border-primary bg-primary bg-opacity-20 pt-2">
+        <div className="w-1/3 flex justify-start" >
+          <LeftComponent title={props.leftTitle} onClick={onClickLeft} />
+        </div>
+        <h1 className="flex justify-center w-1/3 text-sm font-normal" >{props.mainTitle}</h1>
+        <div className="w-1/3 flex justify-end" >
+          <RightComponent title={props.rightTitle} onClick={onClickRight} />
+        </div>
       </div>
     </div>
   );
